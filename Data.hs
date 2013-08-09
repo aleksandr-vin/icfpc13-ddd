98cc48b411028f57194a7081da443c5251475dd6
Murphy812 <murmail@gmail.com>
2013-08-09 22:02:21 +0400

    shows4 Op1, Op2, Expr, Expr', Param

--- Data.hs
+++ Data.hs
@@ -73,8 +73,8 @@ instance Show Expr where
   show (If0 a b c) = "(if0 "++show a++" "++show b++" "++show c++")"
   show (Fold a b c) = "(fold "++show a++" "++show b++" (lambda(y z) ("++show c++"))"
   show (TFold a) = "(fold "++show X++" "++show Zero++" (lambda("++show X++"y) ("++show a++"))"
-  show (Op1 a b) = show a++" "++show b
-  show (Op2 a b c) = show a++" "++show b++" "++show c
+  show (Op1 a b) = "("++show a++" "++show b++")"
+  show (Op2 a b c) = "("++show a++" "++show b++" "++show c++")"
 
 data Operations = OOp1 Op1
                 | OOp2 Op2
--- DataTests.hs
+++ DataTests.hs
@@ -17,9 +17,14 @@ test2 = TestCase (assertEqual "read \"not\""
 test3 = TestCase (assertEqual "read \"if0\""
       OIf0 (read "if0" :: Operations))
 
+testShow1 = TestCase (assertEqual "show not zero"
+      "(not 0)" (show (Data.Op1 Data.Not (Data.P Data.Zero))))
+
+	  
 tests = TestList [TestLabel "test1" test1
-                 ,TestLabel "test2" test2
-                 ,TestLabel "test3" test3
+                  ,TestLabel "test2" test2
+                  ,TestLabel "test3" test3
+                  ,TestLabel "testShow1" testShow1
                  ]
 
 testIt = runTestTT tests
