e2e32b0a43058c1b7c685f9693d899c4b4a9500c
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-08-10 20:43:15 +0400

    Fix style

--- OpenerTests.hs
+++ OpenerTests.hs
@@ -17,16 +17,17 @@ test1 = TestCase (assertEqual "open [(Op1 Not (P Open))]"
                   $ open [(Op1 Not (P Open))])
 
 test2 = TestCase (assertEqual "open [(Op2 And (P Open) (P Open))]"
-                  (nub [(P Zero) -- (Op2 And (P Zero) (P Zero))
-                  ,(P Zero) -- (Op2 And (P Zero) (P One))
-                  ,(P Zero) -- (Op2 And (P Zero) (P X))
---                ,(P Zero) -- (Op2 And (P One) (P Zero))
-                  ,(P One) -- (Op2 And (P One) (P One))
-                  ,(Op2 And (P One) (P X))
---                ,(P Zero) -- (Op2 And (P X) (P Zero))
---                ,(Op2 And (P One) (P X)) -- (Op2 And (P X) (P One))
-                  ,(P X) -- (Op2 And (P X) (P X))
-                  ])
+                  (nub
+                   [(P Zero) -- (Op2 And (P Zero) (P Zero))
+                   ,(P Zero) -- (Op2 And (P Zero) (P One))
+                   ,(P Zero) -- (Op2 And (P Zero) (P X))
+                   ,(P Zero) -- (Op2 And (P One) (P Zero))
+                   ,(P One)  -- (Op2 And (P One) (P One))
+                   ,(Op2 And (P One) (P X))
+                   ,(P Zero) -- (Op2 And (P X) (P Zero))
+                   ,(Op2 And (P One) (P X)) -- (Op2 And (P X) (P One))
+                   ,(P X)    -- (Op2 And (P X) (P X))
+                   ])
                   $ nub $ open [(Op2 And (P Open) (P Open))])
 
 tests = TestList [TestLabel "test1" test1
