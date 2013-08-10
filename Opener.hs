d1084c14b68f7bc7bee551bd9e4c70dedcbb6717
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-08-10 20:40:50 +0400

    Add first-level optimization for Op1 Not & Op2 And operations
    TODO: make op1Comb and op2Comb functions patterns for all other Op1 and Op2 sets

--- Opener.hs
+++ Opener.hs
@@ -10,9 +10,28 @@ open :: [Expr] -> [Expr]
 open exs = concat $ map (openExpr) exs
 
 openExpr :: Expr -> [Expr]
-openExpr (Op1 op e) = map (Op1 op) $ openExpr e
+openExpr (Op1 op e) = map (op1Comb op) $ openExpr e
 openExpr (Op2 op e1 e2) =
-    let ops_e1 = map (Op2 op) $ openExpr e1
+    let ops_e1 = map (op2Comb op) $ openExpr e1
     in concat $ map (\op_e1 -> map (op_e1) $ openExpr e2) ops_e1
 openExpr (P Open) = [(P Zero), (P One), (P X)]
 
+
+-----------------------
+-- Оптимизации Op1
+
+-- Оптимизация Not
+op1Comb Not (P Zero) = (P One)
+op1Comb Not (P One)  = (P Zero)
+op1Comb Not e = (Op1 Not e)
+
+-----------------------
+-- Оптимизации Op2
+
+-- Оптимизация And
+op2Comb And (P Zero) _        = (P Zero)
+op2Comb And (P One)  (P X)    = (Op2 And (P One) (P X))
+op2Comb And (P One)  e        = e
+op2Comb And (P X)    (P Zero) = (P Zero)
+op2Comb And (P X)    (P One)  = op2Comb And (P One) (P X)
+op2Comb And (P X)    (P X)    = (P X)
--- OpenerTests.hs
+++ OpenerTests.hs
@@ -6,25 +6,28 @@ module OpenerTests
 import Data
 import Opener
 import Test.HUnit
+import Data.List
 
 -- Tests
 
 test1 = TestCase (assertEqual "open [(Op1 Not (P Open))]"
-                  [(Op1 Not (P Zero)),(Op1 Not (P One)),(Op1 Not (P X))]
-                  $open [(Op1 Not (P Open))])
+                  [(P One) -- (Op1 Not (P Zero))
+                  ,(P Zero) -- (Op1 Not (P One))
+                  ,(Op1 Not (P X))]
+                  $ open [(Op1 Not (P Open))])
 
 test2 = TestCase (assertEqual "open [(Op2 And (P Open) (P Open))]"
-                  [(Op2 And (P Zero) (P Zero))
-                  ,(Op2 And (P Zero) (P One))
-                  ,(Op2 And (P Zero) (P X))
---                  ,(Op2 And (P One) (P Zero))
-                  ,(Op2 And (P One) (P One))
+                  (nub [(P Zero) -- (Op2 And (P Zero) (P Zero))
+                  ,(P Zero) -- (Op2 And (P Zero) (P One))
+                  ,(P Zero) -- (Op2 And (P Zero) (P X))
+--                ,(P Zero) -- (Op2 And (P One) (P Zero))
+                  ,(P One) -- (Op2 And (P One) (P One))
                   ,(Op2 And (P One) (P X))
---                  ,(Op2 And (P X) (P Zero))
---                  ,(Op2 And (P X) (P One))
-                  ,(Op2 And (P X) (P X))
-                  ]
-                  $ open [(Op2 And (P Open) (P Open))])
+--                ,(P Zero) -- (Op2 And (P X) (P Zero))
+--                ,(Op2 And (P One) (P X)) -- (Op2 And (P X) (P One))
+                  ,(P X) -- (Op2 And (P X) (P X))
+                  ])
+                  $ nub $ open [(Op2 And (P Open) (P Open))])
 
 tests = TestList [TestLabel "test1" test1
                  ,TestLabel "test2" test2
