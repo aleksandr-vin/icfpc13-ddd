cb5e50821ee17d20956467801ac303092481b25e
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-08-10 16:28:51 +0400

    Add Gen.gen function for generating op1, op2 and tfold expressions; add Gen.genP func

--- Gen.hs
+++ Gen.hs
@@ -1,22 +1,47 @@
 
 
 module Gen
-( gen
+( genP
+, gen
 , testIt
 , fooGen
 , fooSizer
+, cons1
+, cons2
 ) where
 
 import Data
 import Test.HUnit
 import Data.List
 
--- import Debug.Trace
+--import Debug.Trace
 trace _ = id
 
-gen :: Int -> [Operations] -> Expr
-gen size ops =
-          (Op1 Not (P Open))
+genP :: Int -> [Operations] -> [Prog]
+genP n o = map (Prog) $ gen n o
+
+gen :: Int -> [Operations] -> [Expr]
+gen n o
+    | n == 1    = [P Open]
+    | otherwise = foldl yyy [] $ filter (\x -> 0 <= (decrN x)) o
+    where yyy acc x = (addHead x) ++ acc
+          addHead x =
+              zzz x
+              where zzz (OOp1 p) = cons1 (Op1 p) (decrN x) o
+                    zzz (OOp2 p) = cons2 (Op2 p) (decrN x) o
+                    zzz (OTFold) = consTFold (TFold) (decrN x) (filter (/= OTFold) o)
+          decrN x = n - (sizer x)
+
+gen' :: Int -> [Operations] -> [Expr']
+gen' n o
+    | n == 1    = [P' (Param Open)]
+    | otherwise = trace (";; +") $ foldl yyy [] $ filter (\x -> 0 <= (decrN x)) o
+    where yyy acc x = (addHead x) ++ acc
+          addHead x =
+              zzz x
+              where zzz (OOp1 p) = cons1' (Op1' p) (decrN x) o
+                    zzz (OOp2 p) = cons2' (Op2' p) (decrN x) o
+          decrN x = n - (sizer x)
 
 -- Для отладки алгоритма перебора
 fooGen :: (Char -> Int) -> Int -> String -> [String]
@@ -34,27 +59,42 @@ fooGen sizer n o
 
 -- Оценщик операции
 sizer OFold = 2
-sizer OTFold = 2
+sizer OTFold = 2 + 1 + 1 -- Мы знаем что в tfold есть X и 0 -- это +2 к цене
 sizer _ = 1
 
 fooSizer :: Char -> Int
 fooSizer n
          | n == 'a'  = 1
          | otherwise = 1
-               
+
+-- коннектор для унарных операций
+--cons1 :: Op1 -> [Expr] -> [Expr]
+consTFold op n o = map op $ gen' n o
+
+cons1 op n o = map op $ gen n o
+
+cons2 op n o = foldl (\acc (e1,e2) -> (mix e1 e2) ++ acc) [] plants
+    where plants = [((gen i o), (gen (n-i) o)) | i <- [1..n `div` 2]] 
+          mix e1 e2 = foldl (\acc e2x -> (map (\e1x -> op e1x e2x) e1) ++ acc) [] e2
+
+cons1' op n o = map op $ gen' n o
+
+cons2' op n o = foldl (\acc (e1,e2) -> (mix e1 e2) ++ acc) [] plants
+    where plants = [((gen' i o), (gen' (n-i) o)) | i <- [1..n `div` 2]] 
+          mix e1 e2 = foldl (\acc e2x -> (map (\e1x -> op e1x e2x) e1) ++ acc) [] e2
 
 -- Tests
 
 test1 = TestCase (assertEqual "fooGenF"
       (Data.List.sort ["aaa"
-                     ,"aab"
-                     ,"aba"
-                     ,"abb"
-                     ,"baa"
-                     ,"bab"
-                     ,"bba"
-                     ,"bbb"
-                     ])
+                      ,"aab"
+                      ,"aba"
+                      ,"abb"
+                      ,"baa"
+                      ,"bab"
+                      ,"bba"
+                      ,"bbb"
+                      ])
       (Data.List.sort (fooGen (fooSizer) 3 "ab")))
 
 tests = TestList [ TestLabel "test1" test1
--- GenTests.hs
+++ GenTests.hs
@@ -10,15 +10,25 @@ import Test.HUnit
 -- Tests
 
 test1 = TestCase (assertEqual "gen 3 [not]"
-      (Op1 Not (P Open))
-      (G.gen 3 [read "not" :: Operations]))
+      [(Op1 Not (P Open))]
+      (G.gen (3-1) $ readOps ["not"]))
 
-test2 = TestCase (assertEqual "gen 5 [and]"
-      (Op2 And (P Open) (P Open))
-      (G.gen 5 [read "and" :: Operations]))
+test2 = TestCase (assertEqual "gen 4 [and]"
+      [(Op2 And (P Open) (P Open))]
+      (G.gen (4-1) $ readOps ["and"]))
+
+test3 = TestCase (assertEqual "gen 4 [and, not]"
+      [(Op1 Not (Op1 Not (P Open))),(Op2 And (P Open) (P Open))]
+      (G.gen (4-1) $ readOps ["and", "not"]))
+
+test4 = TestCase (assertEqual "gen 6 [tfold]"
+      [(TFold (P' (Param Open)))]
+      (G.gen (6-1) $ readOps ["tfold"]))
 
 tests = TestList [TestLabel "test1" test1
                  ,TestLabel "test2" test2
+                 ,TestLabel "test3" test3
+                 ,TestLabel "test4" test4
                  ]
 
 testIt = do runTestTT tests
