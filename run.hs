3eb5c0ab6ec58d35a577ed8ddb00b17b4293d4a5
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-08-10 23:03:03 +0400

    Add list in json result

--- run.hs
+++ run.hs
@@ -11,5 +11,5 @@ run n o = genP (n-1) $ readOps o
 orun n o = openP $ run n o
 
 json progs =
-    encode $ toJSObject [(name, map (JSString . toJSString. show) progs)]
+    encode $ [toJSObject [(name, map (JSString . toJSString. show) progs)]]
     where name = "solutions"
