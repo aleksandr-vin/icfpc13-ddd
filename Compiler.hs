944ddf8a9f61f66c94d12a768c114f45c87e2d7f
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-08-11 19:02:24 +0400

    Add all Op1 and Op2 operations

--- Compiler.hs
+++ Compiler.hs
@@ -22,12 +22,19 @@ compileExp (P Zero)  = (\x -> 0)
 compileExp (P One)   = (\x -> 1)
 compileExp (P X)     = (\x -> x)
 compileExp (Op1 o e) = op1 o $ compileExp e
+compileExp (Op2 o e1 e2) = (\x -> op2 o (compileExp e1 x) (compileExp e2 x))
 
 op1 :: Op1 -> (VType -> VType) -> (VType -> VType)
 op1 Not f = xor basis . f
 op1 Shl1 f = (flip shiftL) 1 . f
 op1 Shr1 f = (flip shiftR) 1 . f
+op1 Shr4 f = (flip shiftR) 4 . f
+op1 Shr16 f = (flip shiftR) 16 . f
 
+op2 And f1 f2 = (.&.) f1 f2
+op2 Or f1 f2 = (.|.) f1 f2
+op2 Xor f1 f2 = xor f1 f2
+op2 Plus f1 f2 = (+) f1 f2
 
 -- Tests
 
--- play.hs
+++ play.hs
@@ -68,6 +68,88 @@ test10 = TestCase (assertEqual ""
                   $ map (foo)
                   [0,1,2,4,0xff,0xFFFFFFFFffffffff])
   where foo = compile (Prog (Op1 Shr1 (P X)))
+        
+test11 = TestCase (assertEqual ""
+                  [0,1,2,4,0xff,0x7FFFFFFFffffffff]
+                  $ map (foo)
+                  [0,1,2,4,0xff,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op1 Shr1 (Op1 Shl1 (P X))))
+        
+test12 = TestCase (assertEqual ""
+                  [0,0,0,0,0x00,0x0f,0x0FFFFFFFffffffff]
+                  $ map (foo)
+                  [0,1,2,4,0x08,0xff,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op1 Shr4 (P X)))
+        
+test13 = TestCase (assertEqual ""
+                  [0,0,0,0,0x00,0x00,0x0000,0x00001,0x0000a0,0x0000FFFFffffffff]
+                  $ map (foo)
+                  [0,1,2,4,0x08,0xff,0xffff,0x10000,0xa00000,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op1 Shr16 (P X)))
+
+test14 = TestCase (assertEqual ""
+                  [0,0,0,0]
+                  $ map (foo)
+                  [0,1,2,4])
+  where foo = compile (Prog (Op2 And (P X) (P Zero)))
+        
+test15 = TestCase (assertEqual ""
+                  [0,1,0,1,0,1,0]
+                  $ map (foo)
+                  [0,1,2,3,4,5,6])
+  where foo = compile (Prog (Op2 And (P X) (P One)))
+        
+test16 = TestCase (assertEqual ""
+                  [0,1,2,4]
+                  $ map (foo)
+                  [0,1,2,4])
+  where foo = compile (Prog (Op2 Or (P X) (P Zero)))
+        
+test17 = TestCase (assertEqual ""
+                  [1,1,3,3,5,5,7]
+                  $ map (foo)
+                  [0,1,2,3,4,5,6])
+  where foo = compile (Prog (Op2 Or (P X) (P One)))
+        
+test18 = TestCase (assertEqual ""
+                   [0,1,2,4,0xff,0xff0ff0,0xFFFFFFFFffffffff]
+                   $ map (foo)
+                   [0,1,2,4,0xff,0xff0ff0,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Xor (P X) (P Zero)))
+        
+test19 = TestCase (assertEqual ""
+                   [1,0,3,5,0xfe,0xff0ff1,0xFFFFFFFFfffffffe]
+                   $ map (foo)
+                   [0,1,2,4,0xff,0xff0ff0,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Xor (P X) (P One)))
+        
+test20 = TestCase (assertEqual ""
+                   [0,0,0,0,0x00,0x000000,0x0000000000000000]
+                   $ map (foo)
+                   [0,1,2,4,0xff,0xff0ff0,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Xor (P X) (P X)))
+        
+test21 = TestCase (assertEqual ""
+                   [0xFFFFFFFFffffffff,0xFFFFFFFFffffffff,0xFFFFFFFFffffffff
+                   ,0xFFFFFFFFffffffff,0xFFFFFFFFffffffff,0xFFFFFFFFffffffff
+                   ,0xFFFFFFFFffffffff]
+                   $ map (foo)
+                   [0,1,2
+                   ,4,0xff,0xff0ff0
+                   ,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Xor (P X) (Op1 Not (P X))))
+        
+test22 = TestCase (assertEqual ""
+                  [1,2,3,4,5,6,7,0x100,0x0000000000000000]
+                  $ map (foo)
+                  [0,1,2,3,4,5,6,0x0ff,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Plus (P X) (P One)))
+        
+test23 = TestCase (assertEqual ""
+                  [0,2,4,6,8,0xa,0xc,0x1fe,0xFFFFFFFFfffffffe]
+                  $ map (foo)
+                  [0,1,2,3,4,0x5,0x6,0x0ff,0xFFFFFFFFffffffff])
+  where foo = compile (Prog (Op2 Plus (P X) (P X)))
 
 
 tests = TestList [TestLabel "test1" test1
@@ -80,6 +162,19 @@ tests = TestList [TestLabel "test1" test1
                  ,TestLabel "test8" test8
                  ,TestLabel "test9" test9
                  ,TestLabel "test10" test10
+                 ,TestLabel "test11" test11
+                 ,TestLabel "test12" test12
+                 ,TestLabel "test13" test13
+                 ,TestLabel "test14" test14
+                 ,TestLabel "test15" test15
+                 ,TestLabel "test16" test16
+                 ,TestLabel "test17" test17
+                 ,TestLabel "test18" test18
+                 ,TestLabel "test19" test19
+                 ,TestLabel "test20" test20
+                 ,TestLabel "test21" test21
+                 ,TestLabel "test22" test22
+                 ,TestLabel "test23" test23
                  ]
 
 play = do testIt
